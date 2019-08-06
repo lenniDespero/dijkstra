49759d087a2b962c071e8faa9cac2da3a5d5ef7d
astepanov <stepanov.alexey@gtrf.ru>
2019-08-06 11:20:19 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Dijkstra graph explorer
+
+### How usage
+clone or download, then `$ composer install`
+
+    <?php
+       require '../vendor/autoload.php';
+
+		use Dijkstra\Graph;
+
+		$routes = file_get_contents('Dijkstra.json');
+		try {
+			$from = 1;
+			$to = 11;
+			$graph = Graph::getGraphByRoutes(json_decode($routes));
+			$result = $graph->getDijkstraRoute($from, $to);
+			// Shortest path from '1' to '11' is: 1->2->5->6->9->10->11. It cost: 38
+			foreach ($result as $toKey => $data) {
+                if ($data['weight'] !== INF) {
+                    echo "Shortest path from '$from' to '$toKey' is: " . implode('->', array_merge($data['path'], [$toKey])) . '. It cost: ' . $data['weight'] . PHP_EOL;
+                } else {
+                    echo "Path from '$from' to '$toKey' is unreachable. . It cost: " . $data['weight'] . PHP_EOL;
+                }
+            }
+    ?>
+    
+More Examples in Example folder
---
+++ src/Dijkstra/Graph.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Dijkstra;
+
+
+/**
+ * Class Graph
+ * @package Dijkstra
+ */
+class Graph
+{
+    /**
+     * @var array
+     */
+    protected $graph;
+
+    /**
+     * Graph constructor.
+     * @param array $graph
+     */
+    public function __construct(array $graph)
+    {
+        $this->graph = $graph;
+    }
+
+    /**
+     * setting up graph by routes
+     * @param $routes
+     * @return Graph
+     * @throws \Exception
+     */
+    public static function getGraphByRoutes($routes)
+    {
+        $graph = array();
+        foreach ($routes as $route) {
+            if (count($route) != 3) {
+                throw new \Exception('Route must have 3 values "from", "to", "weight');
+            }
+            $graph[$route[0]][$route[1]] = $route[2];
+            //We can have nodes without routes from
+            if (!array_key_exists($route[1], $graph)) {
+                $graph[$route[1]] = array();
+            }
+        }
+        return new self($graph);
+    }
+
+    /**
+     * Get shortest path (recursive.. wrong)
+     * @param $from
+     * @param $to
+     * @return mixed
+     * @throws \Exception
+     */
+    public function getDijkstraRoute($from, $to = null)
+    {
+        if (!array_key_exists($from, $this->graph) || (isset($to) && !array_key_exists($to, $this->graph))) {
+            throw new \Exception('Unknown point "from":' . $from . ' or "to":' . $to);
+        }
+        $paths = array();
+        foreach ($this->graph as $node => $weight) {
+            $paths[$node]['weight'] = INF;
+            $paths[$node]['path'] = [];
+        }
+        $paths[$from]['weight'] = 0;
+        $this->calculatePotentials($paths, $from);
+        $paths = array_replace($this->graph, $paths);
+        if (isset($to)) {
+            return [$to => $paths[$to]];
+        }
+        return $paths;
+    }
+
+    /**
+     * Recursive function for calculate potentials
+     * @param $path
+     * @param $currentNodeId
+     */
+    private function calculatePotentials(&$path, $currentNodeId)
+    {
+        foreach ($this->graph[$currentNodeId] as $id => $weight) {
+            $currentWeight = $path[$currentNodeId]['weight'] + $weight;
+            if ($path[$id]['weight'] > $currentWeight && !in_array($id, $path[$currentNodeId]['path'])) {
+                $path[$id]['weight'] = $currentWeight;
+                $path[$id]['path'] = array_merge($path[$currentNodeId]['path'], [$currentNodeId]);
+                $path[$id]['isChecked'] = false;
+            }
+        }
+        $path[$currentNodeId]['isChecked'] = true;
+        foreach ($this->graph[$currentNodeId] as $id => $weight) {
+            if (!$path[$id]['isChecked'] && !in_array($id, $path[$currentNodeId]['path'])) {
+                $this->calculatePotentials($path, $id);
+            }
+        }
+    }
+
+    /**
+     * Get shortest path
+     * @param $from
+     * @param $to
+     * @return mixed
+     * @throws \Exception
+     */
+    public function getAnotherDijkstraRoute($from, $to = null)
+    {
+        if (!array_key_exists($from, $this->graph) || (isset($to) && !array_key_exists($to, $this->graph))) {
+            throw new \Exception('Unknown point "from":' . $from . ' or "to":' . $to);
+        }
+        $paths = array();
+        $list = array();
+        foreach ($this->graph as $node => $weight) {
+            $paths[$node]['weight'] = $list[$node] = INF;
+            $paths[$node]['path'] = [];
+        }
+        $paths[$from]['weight'] = $list[$from] = 0;
+        while ($list) {
+            $min = array_search(min($list), $list);
+            $this->calculateAnotherPotentials($paths, $list, $min);
+            unset($list[$min]);
+        }
+        if (isset($to)) {
+            return [$to => $paths[$to]];
+        }
+        return $paths;
+    }
+
+    /**
+     * Function for calculate potentials
+     * @param $path
+     * @param $list
+     * @param $currentNodeId
+     */
+    private function calculateAnotherPotentials(&$path, &$list, $currentNodeId)
+    {
+        foreach ($this->graph[$currentNodeId] as $id => $weight) {
+            if(!empty($list[$id]) && $list[$currentNodeId] + $weight < $list[$id]) {
+                $list[$id] = $path[$id]['weight'] = $list[$currentNodeId] + $weight;
+                $path[$id]['path'] = array_merge($path[$currentNodeId]['path'],[$currentNodeId]);
+            }
+        }
+    }
+}
